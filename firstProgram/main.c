773463a7ae74a84b66dd043901f292af15fc6805
Andrew Liam C <andrewliamc@startmail.com>
2022-03-22 13:13:47 -0400

    write first basic program
    use the main and printf functions

---
+++ firstProgram/main.c
@@ -0,0 +1,14 @@
+// Practice printing statements using the printf function
+// all statements must go inside the main function
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello World\n");
+  // printf is the function and "Hello World" is an argument, and we are passing the argument into the function
+  // "Hello World" is a string, which is just the combination of characters (known as char)
+  printf("Line 2\n");
+  printf("Lines 3\n");
+  return 0;
+}
