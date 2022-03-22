3f769b2744db85bca5ede430726f22f973941477
Andrew Liam C <andrewliamc@startmail.com>
2022-03-22 15:47:42 -0400

    Create second program
    This is static input process output model program where the variables are initialized to specific values to calculate the distance traveled given a specific time and speed.

---
+++ secondProgram/main.c
@@ -0,0 +1,15 @@
+// ipo model (static)
+
+#include <stdio.h>
+
+int main(void)
+{
+  int speed = 20;
+  int time = 7;
+
+  // An expression
+  int distance = speed * time;
+  printf("Distance: %d\n", distance);
+
+  return 0;
+}
